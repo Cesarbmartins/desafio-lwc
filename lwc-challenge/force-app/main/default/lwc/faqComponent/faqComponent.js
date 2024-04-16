3dafdcc84b074d386091bb4a72e0e7e89b4fc9e5
cesarbmartins <cesar.martins@sptech.school>
2024-04-16 12:20:05 -0300

    Adding pagination to faqComponent

--- lwc-challenge/force-app/main/default/lwc/faqComponent/faqComponent.js
+++ lwc-challenge/force-app/main/default/lwc/faqComponent/faqComponent.js
@@ -1,46 +1,48 @@
-import { LightningElement, wire } from 'lwc';
+import { LightningElement, track, wire } from 'lwc';
 import getFAQs from '@salesforce/apex/FAQController.getFAQs';
 
 const ITEMS_PER_PAGE = 5;
 
 export default class FaqComponent extends LightningElement {
-    faqList = [];
-    displayedFaqs = [];
-    searchText = '';
-    showButton = false;
+    @track faqList = [];
+    @track displayedFaqs = [];
+    @track searchText = '';
+    currentPage = 1;
 
-    @wire(getFAQs)
+    @wire(getFAQs, { searchText: '$searchText', page: '$currentPage' })
     wiredFAQs({ error, data }) {
         if (data) {
-            this.faqList = data.map(faq => ({
-                Id: faq.Id,
-                Question__c: faq.Question__c,
-                Answer__c: faq.Answer__c
-            }));
-            this.displayedFaqs = this.faqList.slice(0, ITEMS_PER_PAGE);
-            if (this.faqList.length > ITEMS_PER_PAGE) {
-                this.showButton = true;
-            }
+            this.faqList = data;
+            this.updateDisplayedFaqs();
         } else if (error) {
             console.error('Erro ao buscar FAQs', error);
         }
     }
 
-    get filteredFaqs() {
-        return this.faqList.filter(faq => 
-            faq.Question__c.toLowerCase().includes(this.searchText.toLowerCase()));
+    get isFirstPage() {
+        return this.currentPage === 1;
+    }
+
+    get isLastPage() {
+        return this.displayedFaqs.length < ITEMS_PER_PAGE;
     }
 
     handleSearch(event) {
         this.searchText = event.target.value;
-        this.displayedFaqs = this.filteredFaqs.slice(0, ITEMS_PER_PAGE);
-        this.showButton = this.filteredFaqs.length > ITEMS_PER_PAGE;
+        this.currentPage = 1;
     }
 
-    handleViewMore() {
-        const currentlyDisplayed = this.displayedFaqs.length;
-        const remainingFaqs = this.filteredFaqs.slice(currentlyDisplayed, currentlyDisplayed + ITEMS_PER_PAGE);
-        this.displayedFaqs = [...this.displayedFaqs, ...remainingFaqs];
-        this.showButton = this.filteredFaqs.length > this.displayedFaqs.length;
+    handleNext() {
+        this.currentPage++;
+    }
+
+    handlePrevious() {
+        this.currentPage--;
+    }
+
+    updateDisplayedFaqs() {
+        const startIndex = (this.currentPage - 1) * ITEMS_PER_PAGE;
+        const endIndex = startIndex + ITEMS_PER_PAGE;
+        this.displayedFaqs = this.faqList.slice(startIndex, endIndex);
     }
 }
